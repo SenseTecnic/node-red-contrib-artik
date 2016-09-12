7a1db279501191b12cc6423e9d2d92238d026e88
Michael Qiu <qiujingming@hotmail.com>
2016-09-12 14:36:34 -0700

    modification to artik.js using nodefn

--- artik.js
+++ artik.js
@@ -1,58 +1,49 @@
 var fs = require('fs');
 var path = require('path');
-
-
+var nodefn = require('when/node');
 
 module.exports = function(RED) {
-  //test if is artik
-
-
 
   function artikNode(config) {
     RED.nodes.createNode(this,config);
     var node = this;
 
-    this.on('input', function(msg){
-      console.log(msg.payload.direction);
-      console.log(typeof(msg.payload.direction));
-      
-      try {
-        if(typeof(msg.payload.direction) !== 'undefinded'){
-          if (msg.payload.direction == 'out'){
-            fs.writeFile("/sys/class/gpio/export", msg.payload.pin, function(){
-              fs.writeFile("/sys/class/gpio/gpio"+ msg.payload.pin +"/direction", msg.payload.direction, function(){
-                fs.writeFile("/sys/class/gpio/gpio"+ msg.payload.pin +"/value", msg.payload.value, function(){
-                  fs.writeFile("/sys/class/gpio/unexport", msg.payload.pin, function(){
-                    console.log(msg);
-                    node.send(msg);
-                  })
-                })
-              })
-            })
-          }
-          if (msg.payload.direction == 'in'){
-            fs.writeFile("/sys/class/gpio/export", msg.payload.pin, function(){
-              fs.writeFile("/sys/class/gpio/gpio"+ msg.payload.pin +"/direction", msg.payload.direction, function(){
-                fs.readFile("/sys/class/gpio/gpio"+ msg.payload.pin +"/value", function read(err, data){
-                  console.log(data.toString('ascii', 0, 1));
-                  msg.payload.value = data.toString('ascii', 0, 1);
-                  fs.writeFile("/sys/class/gpio/unexport", msg.payload.pin, function(){
-                    console.log(msg);
-                    node.send(msg);
-                  })
-                })
-              })
-            })   
-          }
-        } else {
-          msg.payload = {
-            error: "direction is not specified"
-          }
-        }
-      } catch (err){
-        console.log("catch err " + err);
-      }
+    this.on('input', function(msg){     
+
+      if (msg.payload.direction === 'out'){
+        return nodefn.call(fs.writeFile, "/sys/class/gpio/export", msg.payload.pin).then(function(){
+          return nodefn.call(fs.writeFile, "/sys/class/gpio/gpio"+ msg.payload.pin +"/direction", msg.payload.direction);
+        }).then(function(){
+          return nodefn.call(fs.writeFile, "/sys/class/gpio/gpio"+ msg.payload.pin +"/value", msg.payload.value);
+        }).then(function(){
+          return nodefn.call(fs.writeFile, "/sys/class/gpio/unexport", msg.payload.pin);
+        }).then(function(){
+          node.send(msg);
+        }).otherwise(function(err){
+          return node.error(RED._("node-red-contrib-artik.error.unexport-error"));
+        });
+      } else if (msg.payload.direction === 'in'){
+        return nodefn.call(fs.writeFile, "/sys/class/gpio/export", msg.payload.pin).then(function(){
+          return nodefn.call(fs.writeFile, "/sys/class/gpio/gpio"+ msg.payload.pin +"/direction", msg.payload.direction);
+        }).then(function(){
+          return nodefn.call(fs.readFile, "/sys/class/gpio/gpio"+ msg.payload.pin +"/value");
+        }).then(function(data){
+          msg.payload.value = data.toString('ascii', 0, 1);
+          msg.topic = "";
+          return nodefn.call(fs.writeFile, "/sys/class/gpio/unexport", msg.payload.pin);
+        }).then(function(){
+          node.send(msg);
+        }).otherwise(function(err){
+          return node.error(RED._("node-red-contrib-artik.error.unexport-error"));
+        });
+      } else {
+        msg.payload = {
+          error: "direction is not specified"
+        };
+        node.send(msg);
+      }      
     });  
   }
   RED.nodes.registerType("artik",artikNode);
 }
+
